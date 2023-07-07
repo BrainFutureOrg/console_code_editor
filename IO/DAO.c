363b9220a89e82180246abf99adfedda20b5beeb
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-07-07 16:19:27 +0300

    Read / save functions

--- IO/DAO.c
+++ IO/DAO.c
@@ -2,5 +2,52 @@
 // Created by maximus on 03.07.23.
 //
 
+#include <stdio.h>
 #include "DAO.h"
+#include "../prj_error.h"
 
+FILE *open_file_checking(char *filename, char *type)
+{
+    FILE *fp;
+    if (!(fp = fopen(filename, type)))
+    {
+        errno = EACCES;
+    }
+    return fp;
+
+}
+
+string read_file(char *filename)
+{
+    FILE *fp = open_file_checking(filename, "r");
+    if (errno)
+    {
+        string empty = string_create_new(1);
+        return empty;
+    }
+    char c;
+    int filelen = 0;
+    while (((char)getc(fp)) != EOF)
+    {
+        filelen++;
+    }
+    string_fast result_fast = string_fast_create_new(filelen + 1);
+    fseek(fp, SEEK_SET, 0);
+    while ((c = (char)getc(fp)) != EOF)
+    {
+        string_fast_add_char(&result_fast, c);
+    }
+    fclose(fp);
+    return result_fast.string_part;
+}
+void save_file(string containing, char *filename)
+{
+    FILE *fp = open_file_checking(filename, "w+");
+    check_errno_void
+    while (*containing.line != '\0')
+    {
+        putc(*(containing.line++), fp);
+    }
+    fclose(fp);
+
+}
--- IO/DAO.h
+++ IO/DAO.h
@@ -5,4 +5,9 @@
 #ifndef CONSOLE_CODE_EDITOR__DAO_H
 #define CONSOLE_CODE_EDITOR__DAO_H
 
+#include "../prj_types/String_struct.h"
+
+string read_file(char *filename);
+void save_file(string containing, char *filename);
+
 #endif //CONSOLE_CODE_EDITOR__DAO_H

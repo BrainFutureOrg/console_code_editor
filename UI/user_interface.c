35b95253eee5d442a115ff73b3b8d5b7ab32e1f3
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-07-07 14:39:58 +0300

    Use new color functions

--- UI/user_interface.c
+++ UI/user_interface.c
@@ -8,12 +8,8 @@
 
 void print_logo()
 {
-    COLOR foreground_logo = color_create_foreground_rgb(200, 150, 50);
-    COLOR background_logo = color_create_background_rgb(0, 0, 0);
-    printf("%s", background_logo.line);
-    printf("%s", foreground_logo.line);
-    free_color(foreground_logo);
-    free_color(background_logo);
+    color_to_rgb_foreground(200, 150, 50);
+    color_to_rgb_background(0, 0, 0);
 
     printf("C Code ");
     color_inverse();

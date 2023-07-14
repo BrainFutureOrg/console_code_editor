c07b08279c96144a84e954dddfe8daba88ca4fca
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-07-14 13:38:07 +0300

    segments.c formatting

--- IO/segments.c
+++ IO/segments.c
@@ -3,32 +3,47 @@
 //
 
 #include "segments.h"
-void print_string_segment_primitive(string str, COLOR inner_color, urectangle screen_region){
+void print_string_segment_primitive(string str, COLOR inner_color, urectangle screen_region)
+{
     terminal_goto(screen_region.row_start, screen_region.col_start)
-    printf(((string)inner_color).line);
-    for(uint row=screen_region.row_start; row<screen_region.row_end; row++){
-        for(uint col=screen_region.col_start; col<screen_region.col_end; col++){
-            if(*str.line!='\0'){
-                if(*str.line=='\n'){
-                    while(col++<screen_region.col_end)printf(" ");
-                }else{
-                    if(*str.line=='#')color_to_rgb_foreground(255,255,0);
-                    if(*str.line=='{'||*str.line=='}')color_to_rgb_foreground(0,255,0);
+    printf("%s", ((string)inner_color).line);
+    for (uint row = screen_region.row_start; row < screen_region.row_end; row++)
+    {
+        for (uint col = screen_region.col_start; col < screen_region.col_end; col++)
+        {
+            if (*str.line != '\0')
+            {
+                if (*str.line == '\n')
+                {
+                    while (col++ < screen_region.col_end)
+                        printf(" ");
+                }
+                else
+                {
+                    if (*str.line == '#')
+                        color_to_rgb_foreground(255, 255, 0);
+                    if (*str.line == '{' || *str.line == '}')
+                        color_to_rgb_foreground(0, 255, 0);
                     printf("%c", *str.line);
                     str.line++;
                 }
                 //str.line++;
-            }else{
+            }
+            else
+            {
                 printf(" ");
             }
         }
-        if(*str.line=='\n'){
+        if (*str.line == '\n')
+        {
             str.line++;
-        }else{
-            while(*str.line!='\0'&&*str.line++!='\n');
         }
-        color_to_rgb_foreground(255,255,255);
-        terminal_goto(row+1, screen_region.col_start);
+        else
+        {
+            while (*str.line != '\0' && *str.line++ != '\n');
+        }
+        color_to_rgb_foreground(255, 255, 255);
+        terminal_goto(row + 1, screen_region.col_start);
     }
     color_to_default();
 }

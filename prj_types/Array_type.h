99700f30f3d74bc8f0e46f68187cf90330b90f80
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-07-22 18:57:57 +0300

    Struct signature change + create method

--- prj_types/Array_type.h
+++ prj_types/Array_type.h
@@ -11,16 +11,18 @@ typedef struct
 {
     unsigned int size;
     unsigned int allocd_size;
+    void
+    (*free_elements_method)(voidp);
     voidp *elements;
 } array_voidp;
 
-array_voidp array_voidp_create();
+array_voidp array_voidp_create(void (*free_elements_method)(voidp));
 void array_voidp_add_elements_to_end(array_voidp *array, uint number, voidp *elements);
 voidp array_voidp_get_element(array_voidp *array, uint number);
 void array_voidp_delete_element(array_voidp *array, uint number);
 void array_voidp_set_element(array_voidp *array, uint number, voidp element);
 void array_voidp_push(array_voidp *array, voidp element);
 voidp array_voidp_pop(array_voidp *array);
-void free_string_array(array_voidp *array);
+void free_array_voidp(array_voidp *array);
 
 #endif //CONSOLE_CODE_EDITOR_PRJ_TYPES_ARRAY_TYPE_H

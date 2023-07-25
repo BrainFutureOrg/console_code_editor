887affa52deb51ed961a8b488bab2b1d6d97d49a
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-07-25 20:28:55 +0300

    Log files add

---
+++ loging/log.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "log.h"
+#include <stdarg.h>
+#include <time.h>
+
+const char *log_filename_global;
+LogLevel logLevel_global;
+
+void init_logger(LogLevel logLevel, const char *log_filename)
+{
+    log_filename_global = log_filename;
+    logLevel_global = logLevel;
+}
+
+void print_time(FILE *fp)
+{
+    struct tm *tm_info;
+    time_t now;
+    time(&now);
+    tm_info = gmtime(&now);
+    char buffer[26];
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    fputs(buffer, fp);
+}
+
+char *log_to_str(LogLevel logLevel)
+{
+    switch (logLevel)
+    {
+        case FATAL:
+            return "FATAL";
+        case ERROR:
+            return "ERROR";
+        case WARNING:
+            return "WARNING";
+        case INFO:
+            return "INFO";
+        case DEBUG:
+            return "DEBUG";
+    }
+    return NULL;
+}
+
+void write_log(LogLevel logLevel, const char *fmessage, ...)
+{
+    if (logLevel <= logLevel_global)
+    {
+        FILE *fp = fopen(log_filename_global, "a+");
+
+        print_time(fp);
+        fprintf(fp, " %s ", log_to_str(logLevel));
+
+        va_list args;
+        va_start(args, fmessage);
+        vfprintf(fp, fmessage, args);
+        va_end(args);
+
+        putc('\n', fp);
+
+        fclose(fp);
+    }
+}
---
+++ loging/log.h
@@ -0,0 +1,21 @@
+#ifndef CONSOLE_CODE_EDITOR__LOG_H
+#define CONSOLE_CODE_EDITOR__LOG_H
+
+#ifndef LOGFILE_NAME
+#define LOGFILE_NAME "log.txt"
+#endif
+
+typedef enum
+{
+    FATAL,
+    ERROR,
+    WARNING,
+    INFO,
+    DEBUG
+} LogLevel;
+
+void init_logger(LogLevel logLevel, const char *log_filename);
+
+void write_log(LogLevel logLevel, const char *fmessage, ...);
+
+#endif //CONSOLE_CODE_EDITOR__LOG_H

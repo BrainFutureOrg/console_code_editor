988a3c6cb2178fc4948fc08204c77b20fbbd4e7c
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-07-26 17:30:39 +0300

    Add some comments for functions

--- prj_types/Array_type.h
+++ prj_types/Array_type.h
@@ -6,7 +6,9 @@
 #define CONSOLE_CODE_EDITOR_PRJ_TYPES_ARRAY_TYPE_H
 
 #include <sys/types.h>
+/* typedef for developer easier code understanding */
 typedef void *voidp;
+
 typedef struct
 {
     unsigned int size;
@@ -16,13 +18,44 @@ typedef struct
     voidp *elements;
 } array_voidp;
 
+/*
+ * Function to initialize array of void pointers (any pointer type)
+ */
 array_voidp array_voidp_create(void (*free_elements_method)(voidp));
+
+/*
+ * Function that add {number} elements to the end of array
+ */
 void array_voidp_add_elements_to_end(array_voidp *array, uint number, voidp *elements);
+
+/*
+ * Function to get {number} element (counting from 0)
+ */
 voidp array_voidp_get_element(array_voidp *array, uint number);
+
+/*
+ * Function to delete {number} element (counting from 0)
+ */
 void array_voidp_delete_element(array_voidp *array, uint number);
+
+/*
+ * Function to set {number} element instead of the previous (counting from 0)
+ */
 void array_voidp_set_element(array_voidp *array, uint number, voidp element);
+
+/*
+ * Function to push element to the array (add to the end)
+ */
 void array_voidp_push(array_voidp *array, voidp element);
+
+/*
+ * Function to pop element from the array (get the end element and delete it from the array)
+ */
 voidp array_voidp_pop(array_voidp *array);
+
+/*
+ * Function to free all memory allocated for the array
+ */
 void free_array_voidp(array_voidp *array);
 
 #endif //CONSOLE_CODE_EDITOR_PRJ_TYPES_ARRAY_TYPE_H
--- prj_types/String_struct.h
+++ prj_types/String_struct.h
@@ -30,21 +30,76 @@ typedef struct
     string *elements;
 } string_array;
 
+/*
+ * Initialize empty string with {len} allocated memory
+ */
 string string_create_new(uint len);
+
+/*
+ * Initialize string from formatted message (like "%d + %d = %d", 2, 2, 4)
+ * Realized most printf format
+ */
 string string_create_from_fcharp(char *fcharp, ...);
+
+/*
+ * Makes copy of another string
+ */
 string string_copy(string old);
 
+/*
+ * Add char to the end of string function (slow)
+ */
 void string_add_char(string *my_string, char symbol);
+
+/*
+ * Add string to the another string (result saving to the {my_string1})
+ */
 void string_add_string(string *my_string1, string my_string2);
+
+/*
+ * Add char pointer to the string (result saving to the {my_string})
+ */
 void string_add_charp(string *my_string, char *charp);
 
+/*
+ * Free memory allocated to the {my_string}
+ */
 void free_string(string my_string);
 
+/*
+ * Return result of comparing two strings (only lines, without size)
+ */
 char string_equals(string string1, string string2);
+
+/*
+ * Return result of comparing string with char pointer (only lines)
+ */
 char string_charp_equals(string str, char *charp);
 
+/*
+ * Convert int to string
+ */
 string int_to_string(int num);
+
+/*
+ * Convert double to string
+ */
 string double_to_string(double num, int fractional_symbol_num);
+
+/*
+ * Copy of zfill python function
+ * arguments: my_string - string for operation
+ *            size      - the minimum size of the line
+ *            sign      - place where {symbol} should paste
+ *                        0     - to the start of the line
+ *                        other - to the end of the line
+ *            symbol    - symbol for fill line to the {size}
+ * result is writing to the {my_string}
+ * example:
+ *            string s1 = string_create_from_fcharp("Hello");
+ *            string_zfill(s1, 10, 0, ' ');
+ *            s1.line; // "     Hello"
+ */
 void string_zfill(string *my_string, int size, char sign, char symbol);
 
 string_fast string_fast_create_new(uint len);
@@ -72,15 +127,21 @@ string string_array_pop(string_array *array);
 void string_array_print(string_array *array, char type);
 void free_string_array(string_array *array);
 
-// insert character into string, string is viewed as a text with lines ended with '\n'
+/*
+ * insert character into string, string is viewed as a text with lines ended with '\n'
+ */
 void insert_into_string_multiline(string *insert_into, char c, uint row, uint col);
-// delete character from string, string is viewed as a text with lines ended with '\n'
+/*
+ * delete character from string, string is viewed as a text with lines ended with '\n'
+ */
 void delete_from_string_multiline(string *delete_from, uint row, uint col);
 
-/* Split function
+/*
+ * Split function
  * arguments: my_string - pointer to the start of line
  *            split_func - function that return number of symbols that fit else 0
- * return: array of split strings*/
+ * return: array of split strings
+ */
 string_array string_split(char *my_string, char (*split_func)(char *));
 
 char string_split_is_space(char *charp);

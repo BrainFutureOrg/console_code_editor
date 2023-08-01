cd477d284103121ccb40f0293712d7fd1993bb00
MaksymShkarupylo <shkarupilo2005@gmail.com>
2023-08-01 21:29:06 +0300

    add functions realizations

--- src/file_system/file_system_work.c
+++ src/file_system/file_system_work.c
@@ -8,6 +8,8 @@
 #include "../IO/DAO.h"
 #include <dirent.h>
 
+#define dir_slash '/'
+
 /*
  * Get all content without . and ..
  */
@@ -73,7 +75,7 @@ string system_anchor_get_full_path(file_system_anchor anchor)
 
 void system_anchor_go_to_dir(file_system_anchor *anchor, string directory_name)
 {
-    string_add_char(&anchor->path, '/');
+    string_add_char(&anchor->path, dir_slash);
     string_add_string(&anchor->path, directory_name);
 }
 
@@ -86,7 +88,7 @@ void system_anchor_go_back_from_dir(file_system_anchor *anchor)
     }
     while (*++end != '\0');
     //while (*--end != '\\');
-    while (*--end != '/' && *end != '\\');
+    while (*--end != dir_slash);
     *end = '\0';
 }
 #include "../loging/log.h"
@@ -102,7 +104,7 @@ string system_anchor_get_directory_name(file_system_anchor *anchor)
     //write_log(DEBUG, "after \\0 check");
     while (*++end != '\0');
     //write_log(DEBUG, "c1 end");
-    while (*--end != '/' && *end != '\\');//
+    while (*--end != dir_slash);
     //write_log(DEBUG, "c2 now = %c", *end);
     //write_log(DEBUG, "c2 end");
     return string_create_from_fcharp(end + 1);
@@ -144,7 +146,52 @@ void anchor_save_file(file_system_anchor anchor, char *filename, string contains
 string path_join(string path, char *filename)
 {
     string path_to_file = string_copy(path);
-    string_add_char(&path_to_file, '/');
+    string_add_char(&path_to_file, dir_slash);
     string_add_charp(&path_to_file, filename);
     return path_to_file;
+}
+
+void anchor_create_dir(file_system_anchor anchor, char *dir_name)
+{
+    string path_to_dir = path_join(anchor.path, dir_name);
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        write_log(ERROR, "Can`t clone process, error is: %s", strerror(errno));
+        return;
+    }
+    if (pid == 0)
+    {
+        if (execlp("mkdir", "mkdir", path_to_dir.line, NULL) == -1)
+        {
+            write_log(ERROR, "Can`t create dir with mkdir: %s", strerror(errno));
+        }
+    }
+}
+
+char anchor_is_file_exist(file_system_anchor anchor, char *file_name)
+{
+    files_dirs_from_directory files_dirs = system_anchor_get_dir_content(anchor);
+    char result = string_array_is_charp_in(&files_dirs.files, file_name) ||
+        string_array_is_charp_in(&files_dirs.dirs, file_name) ||
+        string_array_is_charp_in(&files_dirs.else_files, file_name);
+    free_files_dirs_from_directory(files_dirs);
+    return result;
+}
+
+char is_file_symbol_valid(char c)
+{
+    return c == dir_slash;
+}
+
+char is_file_name_valid(char *file_name)
+{
+    while (*file_name != '\0')
+    {
+        if (is_file_symbol_valid(*file_name++))
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
